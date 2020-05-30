139794aac6a94ff76634650f294b50f64d258d7c
Mark Ryall <mark@ryall.name>
2020-05-30 14:55:27 +1000

    browse to github and semaphore from workflow

--- lib/semaph/shells/workflow/workflow_shell.rb
+++ lib/semaph/shells/workflow/workflow_shell.rb
@@ -1,3 +1,4 @@
+require "semaph/commands/visit_url_command"
 require "semaph/commands/reload_command"
 require "semaph/shells/workflow/pipelines_list_command"
 require "semaph/shells/workflow/pipelines_select_command"
@@ -17,8 +18,44 @@ module Semaph
           add_command PipelinesListCommand.new(pipeline_collection), "ls"
           add_command PipelinesSelectCommand.new(pipeline_collection), "cd"
           add_command(
+            ::Semaph::Commands::VisitUrlCommand.new(
+              "https://#{project.client.host}/workflows/#{workflow.id}",
+              "browse to workflow",
+            ),
+            "open-workflow",
+          )
+          add_command(
+            ::Semaph::Commands::VisitUrlCommand.new(
+              "https://#{project.client.host}/branches/#{workflow.branch_id}",
+              "browse to branch in semaphore",
+            ),
+            "open-branch",
+          )
+          add_github_commands(workflow)
+          add_command(
             ::Semaph::Commands::ReloadCommand.new(pipeline_collection, "reload pipelines"),
             "reload",
+          )
+        end
+
+        private
+
+        def add_github_commands(workflow)
+          return unless workflow.project.github_url
+
+          add_command(
+            ::Semaph::Commands::VisitUrlCommand.new(
+              "#{workflow.project.github_url}/tree/#{workflow.branch}",
+              "browse to the branch in github",
+            ),
+            "github-branch",
+          )
+          add_command(
+            ::Semaph::Commands::VisitUrlCommand.new(
+              "#{workflow.project.github_url}/commit/#{workflow.sha}",
+              "browse to the commit in github",
+            ),
+            "github-commit",
           )
         end
       end
