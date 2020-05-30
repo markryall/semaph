dbedb283392526dfaa5c4dea3be305c4f97879e2
Mark Ryall <mark@ryall.name>
2020-05-30 11:22:48 +1000

    Skip organisations shell if there's only one

--- lib/semaph.rb
+++ lib/semaph.rb
@@ -1,5 +1,6 @@
 require "semaph/version"
 require "semaph/shells/organisations/organisations_shell"
+require "semaph/shells/organisation/organisation_shell"
 require "yaml"
 
 module Semaph
@@ -10,6 +11,12 @@ module Semaph
     raise "Please install the sem tool and authenticate to semaphoreci.com" unless File.exist?(yaml_path)
 
     sem_config = YAML.load_file(yaml_path)
-    Shells::Organisations::OrganisationsShell.new(sem_config["contexts"]).push
+    contexts = sem_config["contexts"]
+
+    if contexts.count == 1
+      Shells::Organisation::OrganisationShell.new(contexts.values.first).push
+    else
+      Shells::Organisations::OrganisationsShell.new(contexts).push
+    end
   end
 end
