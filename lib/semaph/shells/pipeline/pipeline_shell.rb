f6d48d05187bc6602e97c5e7d8645a79cd26a580
Mark Ryall <mark@ryall.name>
2020-06-01 22:02:17 +1000

    add the same workflow links to pipelines

--- lib/semaph/shells/pipeline/pipeline_shell.rb
+++ lib/semaph/shells/pipeline/pipeline_shell.rb
@@ -36,7 +36,57 @@ module Semaph
           @jobs_list_command = JobsListCommand.new(job_collection)
           add_command @jobs_list_command, "list-jobs"
           add_command JobsPollCommand.new(job_collection), "poll-jobs"
+          add_open_branch_command
+          add_open_workflow_command
+          add_github_commands
           add_command ::Semaph::Commands::ReloadCommand.new, "reload" if ENV["SEMAPH_RELOAD"]
+        end
+
+        def add_open_workflow_command
+          add_command(
+            ::Semaph::Commands::VisitUrlCommand.new(
+              "https://#{project.client.host}/workflows/#{workflow.id}",
+              "browse to workflow",
+            ),
+            "open-workflow",
+          )
+        end
+
+        def add_open_branch_command
+          add_command(
+            ::Semaph::Commands::VisitUrlCommand.new(
+              "https://#{project.client.host}/branches/#{workflow.branch_id}",
+              "browse to branch in semaphore",
+            ),
+            "open-branch",
+          )
+        end
+
+        def add_github_commands
+          return unless workflow.project.github_url
+
+          add_github_branch
+          add_github_commit
+        end
+
+        def add_github_branch
+          add_command(
+            ::Semaph::Commands::VisitUrlCommand.new(
+              "#{workflow.project.github_url}/tree/#{workflow.branch}",
+              "browse to the branch in github",
+            ),
+            "open-github-branch",
+          )
+        end
+
+        def add_github_commit
+          add_command(
+            ::Semaph::Commands::VisitUrlCommand.new(
+              "#{workflow.project.github_url}/commit/#{workflow.sha}",
+              "browse to the commit in github",
+            ),
+            "open-github-commit",
+          )
         end
       end
     end
